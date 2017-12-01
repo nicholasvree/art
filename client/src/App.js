905f5a7a6c991297811d0185c5aec6ec75e2da0a
Nicholas Vree <nicholasvree@Nicholass-MacBook-Air.local>
2017-11-30 22:00:12 -0600

    WIP

--- client/src/App.js
+++ client/src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
     userEmail:null,
     userId: null,
   }
+  
 ////////////////////SHARED////////////////////////////
 
   componentDidMount(){ 
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -23,7 +23,7 @@ class Login extends React.Component {
 
     return (
         <div className="login">
-        <h1>Login to Web App</h1>
+        <h1>Login to Web App --- ISSUE - ENTER USERNAME AND EMAIL AS THE SAME THING</h1>
           <p><input type="text" name="loginEmail" value={this.state.email} onChange={this.props.handleInputChange} placeholder="Email"/></p>
           <p><input type="password" name="loginPassword" value={this.state.password} onChange={this.props.handleInputChange} placeholder="Password"/></p>
           <p className="submit"><input type="submit" name="commit" value="Login" onClick={this.props.handleLoginSubmit}/></p>
